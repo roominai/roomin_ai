46846552ef6a56fac0fa9312c39df79fdb454dde
roominai <contato.roomin.ai@gmail.com>
2025-04-23 02:03:48 -0300

    Update DropDown.tsx

--- components/DropDown.tsx
+++ components/DropDown.tsx
@@ -5,6 +5,7 @@ import {
   ChevronUpIcon,
 } from "@heroicons/react/20/solid";
 import { Fragment } from "react";
+import { roomTranslations, themeTranslations } from "../utils/dropdownTypes";
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
@@ -21,7 +22,7 @@ export default function DropDown({ theme, setTheme, themes }: DropDownProps) {
     <Menu as="div" className="relative block text-left w-full">
       <div>
         <Menu.Button className="inline-flex w-full justify-between items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-500">
-          {theme}
+          {themeTranslations[theme as keyof typeof themeTranslations] || roomTranslations[theme as keyof typeof roomTranslations] || theme}
           <ChevronUpIcon
             className="-mr-1 ml-2 h-5 w-5 ui-open:hidden text-gray-500"
             aria-hidden="true"
@@ -58,7 +59,7 @@ export default function DropDown({ theme, setTheme, themes }: DropDownProps) {
                       "px-4 py-2 text-sm w-full text-left flex items-center space-x-2 justify-between"
                     )}
                   >
-                    <span>{themeItem}</span>
+                    <span>{themeTranslations[themeItem as keyof typeof themeTranslations] || roomTranslations[themeItem as keyof typeof roomTranslations] || themeItem}</span>
                     {theme === themeItem ? (
                       <CheckIcon className="w-4 h-4 text-blue-600" />
                     ) : null}
