8e636b08efd2e076c9afd1278ab254a44021d2f2
roominai <contato.roomin.ai@gmail.com>
2025-04-22 23:20:01 -0300

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -2,6 +2,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://jyxkboiztqnrtorwkowp.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp5eGtib2l6dHFucnRvcndrb3dwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUzNzAxODAsImV4cCI6MjA2MDk0NjE4MH0.B3uu6HayJJu7OtX13UMHSKh3mQJaxqCsFD8Yvtc5zYY';
+const supabaseKey = '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';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
