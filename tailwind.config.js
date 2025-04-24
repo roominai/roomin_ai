588fa26c08ba916de9196864018d36041f7664a9
roominai <contato.roomin.ai@gmail.com>
2025-04-23 22:40:56 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,14 @@ module.exports = {
       screens: {
         xs: "330px",
       },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      fontSize: {
+        '4xl': ['2.25rem', { lineHeight: '2.5rem', letterSpacing: '-0.02em' }],
+        '6xl': ['3.75rem', { lineHeight: '1', letterSpacing: '-0.02em' }],
+      },
     },
   },
   plugins: [require("@tailwindcss/forms"), require("@headlessui/tailwindcss")],
