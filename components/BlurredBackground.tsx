e643dda3a25a386832ff3dae0a5ee486f1f88284
roominai <contato.roomin.ai@gmail.com>
2025-04-28 01:48:40 -0300

    Create BlurredBackground.tsx

---
+++ components/BlurredBackground.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const BlurredBackground: React.FC = () => {
+  return (
+    <div className="blurred-spots-background">
+      <div className="blur-spot blur-spot-1"></div>
+      <div className="blur-spot blur-spot-2"></div>
+      <div className="blur-spot blur-spot-3"></div>
+      <div className="blur-spot blur-spot-4"></div>
+      <div className="blur-spot blur-spot-5"></div>
+    </div>
+  );
+};
+
+export default BlurredBackground;
